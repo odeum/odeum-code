2aab14c7ee2f11e136edc5e063092866e3c97049
Alexand3r <andrei.tudor27@yahoo.com>
2017-04-27 10:40:34 +0200

    Fix index.js of container home

--- experiments/3/src/containers/Home/index.js
+++ experiments/3/src/containers/Home/index.js
@@ -1,15 +1,11 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-import {HomeDiv} from './styled'
 
 //Header+Drawer
 
-import React, { Component } from 'react'
 //import * as s from './styled'
 import {HomeDiv} from './styled'
-// eslint-disable-next-line
-import Home from '../../components/Home'
 
 import HeaderContainer from './Header'
 import DrawerContainer from './Drawer'
