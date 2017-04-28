a4c82c71cbddff29b6852f103f15cc1eb854fed3
Alexand3r <andrei.tudor27@yahoo.com>
2017-04-28 13:28:45 +0200

    New Tabs Wrapper

--- experiments/3/src/components/Tabs/TabsWrapper.js
+++ experiments/3/src/components/Tabs/TabsWrapper.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types'
 class TabsWrapper extends Component {
+    static propTypes = {
+        children:PropTypes.array
+    }
     render() {
         return (
             <div style={{background:'red',height:50, marginTop:-21}}>
