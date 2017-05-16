7d55029e64eb06e2d12231332a9c1f1a5aa9c76d
Christian Broberg <cb@webhouse.dk>
2017-05-16 23:18:58 +0200

    Sign in of 99 icons ready to rock

--- experiments/theme/src/App.js
+++ experiments/theme/src/App.js
@@ -8,6 +8,7 @@ import * as colors from './components/colors'
 
 const Text = styled.p`
   margin: 1rem 1rem
+  font-size: 1.5rem
   font-family: ${(props) => props.theme.font || 'sans-serif'}
 `
 
@@ -34,6 +35,16 @@ class App extends Component {
           <Icon icon={iconnames.ICON_ASSIGNMENT} size={50} color={colors.TAB_COLOR_7} active={true} />
           <Icon icon={iconnames.ICON_ASSIGNMENT_TURNED_IN} size={50} color={colors.TAB_COLOR_8} active={true} />
           <Icon icon={iconnames.ICON_PEOPLE} size={50} color={colors.TAB_COLOR_9} active={true} />
+          <Icon icon={iconnames.ICON_GROUP_ADD} size={50} color={colors.TAB_COLOR_10} active={true} />
+          <Icon icon={iconnames.ICON_PERSON} size={50} color={colors.TAB_COLOR_1} active={true} />
+          <Icon icon={iconnames.ICON_PERSON_ADD} size={50} color={colors.TAB_COLOR_3} active={true} />
+          <Icon icon={iconnames.ICON_MAIL_OUTLINE} size={50} color={colors.TAB_COLOR_4} active={true} />
+          <Icon icon={iconnames.ICON_SETTINGS_CELL} size={50} color={colors.TAB_COLOR_5} active={true} />
+          <Icon icon={iconnames.ICON_EXTENSION} size={50} color={colors.TAB_COLOR_6} active={true} />
+          <Icon icon={iconnames.ICON_ARROW_DROP_DOWN} size={50} color={colors.TAB_COLOR_7} active={true} />
+          <Icon icon={iconnames.ICON_ARROW_DROP_UP} size={50} color={colors.TAB_COLOR_8} active={true} />
+          <Icon icon={iconnames.ICON_COPYRIGHT} size={50} color={colors.TAB_COLOR_9} active={true} />
+
           <Icon icon={iconnames.ICON_CLOUD} size={50} color={colors.TAB_COLOR_1} active={true} />
           <Icon icon={iconnames.ICON_CLOUD_DOWNLOAD} size={50} color={colors.TAB_COLOR_2} active={true} />
           <Icon icon={iconnames.ICON_CLOUD_UPLOAD} size={50} color={colors.TAB_COLOR_3} active={true} />
@@ -44,6 +55,40 @@ class App extends Component {
           <Icon icon={iconnames.ICON_OPACITY} size={50} color={colors.TAB_COLOR_4} active={true} />
           <Icon icon={iconnames.ICON_LANGUAGE} size={50} color={colors.TAB_COLOR_5} active={true} />
           <Icon icon={iconnames.ICON_DATE_RANGE} size={50} color={colors.TAB_COLOR_6} active={true} />
+          <Icon icon={iconnames.ICON_CANCEL} size={50} color={colors.TAB_COLOR_8} active={true} />
+          <Icon icon={iconnames.ICON_MODE_EDIT} size={50} color={colors.TAB_COLOR_9} active={true} />
+          <Icon icon={iconnames.ICON_PLACE} size={50} color={colors.TAB_COLOR_10} active={true} />
+
+          <Icon icon={iconnames.ICON_PHONE} size={50} color={colors.TAB_COLOR_4} active={true} />
+          <Icon icon={iconnames.ICON_VISIBILITY} size={50} color={colors.TAB_COLOR_5} active={true} />
+          <Icon icon={iconnames.ICON_VISIBILITY_OFF} size={50} color={colors.TAB_COLOR_6} active={true} />
+          <Icon icon={iconnames.ICON_ACCOUNT_BOX} size={50} color={colors.TAB_COLOR_7} active={true} />
+          <Icon icon={iconnames.ICON_INSERT_DRIVE_FILE} size={50} color={colors.TAB_COLOR_8} active={true} />
+          <Icon icon={iconnames.ICON_VIEW_HEADLINE} size={50} color={colors.TAB_COLOR_9} active={true} />
+          <Icon icon={iconnames.ICON_VIEW_MODULE} size={50} color={colors.TAB_COLOR_10} active={true} />
+          <Icon icon={iconnames.ICON_SHARE} size={50} color={colors.TAB_COLOR_1} active={true} />
+          <Icon icon={iconnames.ICON_FAVORITE} size={50} color={colors.TAB_COLOR_2} active={true} />
+          <Icon icon={iconnames.ICON_FAVORITE_BORDER} size={50} color={colors.TAB_COLOR_3} active={true} />
+          <Icon icon={iconnames.ICON_LABEL} size={50} color={colors.TAB_COLOR_4} active={true} />
+          <Icon icon={iconnames.ICON_LABEL_OUTLINE} size={50} color={colors.TAB_COLOR_5} active={true} />
+          <Icon icon={iconnames.ICON_STAR} size={50} color={colors.TAB_COLOR_6} active={true} />
+          <Icon icon={iconnames.ICON_STARS} size={50} color={colors.TAB_COLOR_7} active={true} />
+          <Icon icon={iconnames.ICON_TODAY} size={50} color={colors.TAB_COLOR_8} active={true} />
+          <Icon icon={iconnames.ICON_PLAY_CIRCLE_FILLED} size={50} color={colors.TAB_COLOR_9} active={true} />
+          <Icon icon={iconnames.ICON_PLAY_CIRCLE_OUTLINE} size={50} color={colors.TAB_COLOR_10} active={true} />
+          <Icon icon={iconnames.ICON_SKIP_NEXT} size={50} color={colors.TAB_COLOR_1} active={true} />
+          <Icon icon={iconnames.ICON_SKIP_PREVIOUS} size={50} color={colors.TAB_COLOR_2} active={true} />
+          <Icon icon={iconnames.ICON_PIE_CHART} size={50} color={colors.TAB_COLOR_3} active={true} />
+          <Icon icon={iconnames.ICON_PIE_CHART_OUTLINED} size={50} color={colors.TAB_COLOR_4} active={true} />
+          <Icon icon={iconnames.ICON_SHOW_CHART} size={50} color={colors.TAB_COLOR_5} active={true} />
+          <Icon icon={iconnames.ICON_INSERT_CHART} size={50} color={colors.TAB_COLOR_6} active={true} />
+          <Icon icon={iconnames.ICON_EQUALIZER} size={50} color={colors.TAB_COLOR_7} active={true} />
+          <Icon icon={iconnames.ICON_WEB_ASSET} size={50} color={colors.TAB_COLOR_8} active={true} />
+          <Icon icon={iconnames.ICON_WEB} size={50} color={colors.TAB_COLOR_9} active={true} />
+          <Icon icon={iconnames.ICON_TABLET_MAC} size={50} color={colors.TAB_COLOR_10} active={true} />
+          <Icon icon={iconnames.ICON_LAPTOP_MAC} size={50} color={colors.TAB_COLOR_1} active={true} />
+          <Icon icon={iconnames.ICON_DESKTOP_MAC} size={50} color={colors.TAB_COLOR_2} active={true} />
+          <Icon icon={iconnames.ICON_PHONE_IPHONE} size={50} color={colors.TAB_COLOR_3} active={true} />
 
           <Icon icon={iconnames.ICON_MESSAGES} size={50} color={colors.EMERALD_LIGHT} active={true} />
           <Icon icon={iconnames.ICON_HELP} size={50} color={colors.EMERALD_LIGHT} active={true} />
@@ -56,7 +101,29 @@ class App extends Component {
           <Icon icon={iconnames.ICON_KEYBOARD_ARROW_UP} size={50} color={colors.EMERALD_DARK} active={true} />
           <Icon icon={iconnames.ICON_KEYBOARD_ARROW_LEFT} size={50} color={colors.EMERALD_DARK} active={true} />
           <Icon icon={iconnames.ICON_KEYBOARD_ARROW_RIGHT} size={50} color={colors.EMERALD_DARK} active={true} />
+          <Icon icon={iconnames.ICON_ADD_CIRCLE_OUTLINE} size={50} color={colors.EMERALD_DARK} active={true} />
+          <Icon icon={iconnames.ICON_DELETE} size={50} color={colors.DREAMY_BLUE} active={true} />
+          <Icon icon={iconnames.ICON_DELETE_FOREVER} size={50} color={colors.TAB_COLOR_7} active={true} />
+          <Icon icon={iconnames.ICON_CONTENT_COPY} size={50} color={colors.TAB_COLOR_7} active={true} />
+          <Icon icon={iconnames.ICON_BUG_REPORT} size={50} color={colors.TAB_COLOR_8} active={true} />
 
+          <Icon icon={iconnames.ICON_PHOTO_CAMERA} size={50} color={colors.TAB_COLOR_8} active={true} />
+          <Icon icon={iconnames.ICON_SHORT_TEXT} size={50} color={colors.TAB_COLOR_9} active={true} />
+          <Icon icon={iconnames.ICON_EVENT} size={50} color={colors.TAB_COLOR_10} active={true} />
+          <Icon icon={iconnames.ICON_LIST} size={50} color={colors.TAB_COLOR_1} active={true} />
+          <Icon icon={iconnames.ICON_WORK} size={50} color={colors.TAB_COLOR_2} active={true} />
+          <Icon icon={iconnames.ICON_ACCESS_TIME} size={50} color={colors.TAB_COLOR_3} active={true} />
+          <Icon icon={iconnames.ICON_MIC} size={50} color={colors.TAB_COLOR_4} active={true} />
+          <Icon icon={iconnames.ICON_VIDEOCAM} size={50} color={colors.TAB_COLOR_5} active={true} />
+          <Icon icon={iconnames.ICON_CALL} size={50} color={colors.TAB_COLOR_6} active={true} />
+          <Icon icon={iconnames.ICON_MESSAGE} size={50} color={colors.TAB_COLOR_7} active={true} />
+          <Icon icon={iconnames.ICON_FILTER_LIST} size={50} color={colors.TAB_COLOR_8} active={true} />
+          <Icon icon={iconnames.ICON_ATTACH_FILE} size={50} color={colors.TAB_COLOR_9} active={true} />
+          <Icon icon={iconnames.ICON_DIRECTIONS} size={50} color={colors.TAB_COLOR_10} active={true} />
+          <Icon icon={iconnames.ICON_MOOD} size={50} color={colors.TAB_COLOR_1} active={true} />
+          <Icon icon={iconnames.ICON_CHECK_BOX} size={50} color={colors.TAB_COLOR_2} active={true} />
+          <Icon icon={iconnames.ICON_RADIO_BUTTON_CHECKED} size={50} color={colors.TAB_COLOR_3} active={true} />
+          <Icon icon={iconnames.ICON_GRID_ON} size={50} color={colors.TAB_COLOR_4} active={true} />
 
           <Text>Testing default props ... </Text>
           <Icon icon={iconnames.ICON_SEARCH} size={50} active={true} />
@@ -64,10 +131,11 @@ class App extends Component {
           <Icon icon={iconnames.ICON_CLOSE} color={colors.PRIMARY_BLUE} active={true} />
           <Icon icon={iconnames.ICON_CHECK} color={colors.PRIMARY_BLUE} active={true} />
           <Icon icon={iconnames.ICON_CHECK_CIRCLE} color={colors.MENUBAR_SELECTED} active={true} />
+          <Icon icon={iconnames.ICON_ADD_CIRCLE} color={colors.PRIMARY_BLUE} active={true} />
 
           <Icon icon='close' size={50} color={colors.DREAMY_BLUE} active={true} />
           <Icon icon='notifications' active={true} />
-          <Icon icon='' active={true} />
+          <Icon icon='' size={50} active={true} />
           <Icon icon='' color={colors.DREAMY_BLUE} active={true} />
           <Icon />
 
--- experiments/theme/src/components/Icon.js
+++ experiments/theme/src/components/Icon.js
@@ -30,11 +30,7 @@ getSubComponent(name, size) {
 // All theme available icons must be in this switch
 
 switch (name) {
-    case iconnames.ICON_SETTINGS: 
-    return <Icons.MdSettings 
-                size={size || this.defaultProps.size} 
-                color={this.setColor(this.props.active) || this.defaultProps.color} />
-                
+    case iconnames.ICON_SETTINGS: return <Icons.MdSettings size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_MENU: return <Icons.MdMenu size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_INPUT: return <Icons.MdInput size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_TIMELINE: return <Icons.MdTimeline size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
@@ -45,13 +41,53 @@ switch (name) {
     case iconnames.ICON_ASSIGNMENT: return <Icons.MdAssignment size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_ASSIGNMENT_TURNED_IN: return <Icons.MdAssignmentTurnedIn size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_PEOPLE: return <Icons.MdPeople size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_GROUP_ADD: return <Icons.MdGroupAdd size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PERSON: return <Icons.MdPerson size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PERSON_ADD: return <Icons.MdPersonAdd size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_MAIL_OUTLINE: return <Icons.MdMailOutline size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_SETTINGS_CELL: return <Icons.MdSettingsCell size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_EXTENSION: return <Icons.MdExtension size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_ARROW_DROP_DOWN: return <Icons.MdArrowDropDown size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_ARROW_DROP_UP: return <Icons.MdArrowDropUp size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_COPYRIGHT: return <Icons.MdCopyright size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_CLOUD: return <Icons.MdCloud size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_CLOUD_DOWNLOAD: return <Icons.MdCloudDownload size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_CLOUD_UPLOAD: return <Icons.MdCloudUpload size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_APPS: return <Icons.MdApps size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_LANGUAGE: return <Icons.MdLanguage size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_DATE_RANGE: return <Icons.MdDateRange size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
-
+    case iconnames.ICON_MODE_EDIT: return <Icons.MdModeEdit size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PLACE: return <Icons.MdPlace size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PHONE: return <Icons.MdPhone size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_VISIBILITY: return <Icons.MdVisibility size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_VISIBILITY_OFF: return <Icons.MdVisibilityOff size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_ACCOUNT_BOX: return <Icons.MdAccountBox size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_INSERT_DRIVE_FILE: return <Icons.MdInsertDriveFile size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_VIEW_HEADLINE: return <Icons.MdViewHeadline size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_VIEW_MODULE: return <Icons.MdViewModule size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_SHARE: return <Icons.MdShare size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_FAVORITE: return <Icons.MdFavorite size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_FAVORITE_BORDER: return <Icons.MdFavoriteBorder size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_LABEL: return <Icons.MdLabel size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_LABEL_OUTLINE: return <Icons.MdLabelOutline size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_STAR: return <Icons.MdStar size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_STARS: return <Icons.MdStars size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_TODAY: return <Icons.MdToday size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PLAY_CIRCLE_FILLED: return <Icons.MdPlayCircleFilled size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PLAY_CIRCLE_OUTLINE: return <Icons.MdPlayCircleOutline size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_SKIP_NEXT: return <Icons.MdSkipNext size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_SKIP_PREVIOUS: return <Icons.MdSkipPrevious size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PIE_CHART: return <Icons.MdPieChart size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PIE_CHART_OUTLINED: return <Icons.MdPieChartOutlined size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_SHOW_CHART: return <Icons.MdShowChart size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_INSERT_CHART: return <Icons.MdInsertChart size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_EQUALIZER: return <Icons.MdEqualizer size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_WEB_ASSET: return <Icons.MdWebAsset size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_WEB: return <Icons.MdWeb size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_TABLET_MAC: return <Icons.MdTabletMac size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_LAPTOP_MAC: return <Icons.MdLaptopMac size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_DESKTOP_MAC: return <Icons.MdDesktopMac size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PHONE_IPHONE: return <Icons.MdPhoneIphone size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_SEARCH: return <Icons.MdSearch size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_MESSAGES: return <Icons.MdChat size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_HELP: return <Icons.MdHelp size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} /> 
@@ -61,6 +97,12 @@ switch (name) {
     case iconnames.ICON_CLOSE: return <Icons.MdClose size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_CHECK: return <Icons.MdCheck size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_CHECK_CIRCLE: return <Icons.MdCheckCircle size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_CANCEL: return <Icons.MdCancel size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_ADD_CIRCLE: return <Icons.MdAddCircle size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_ADD_CIRCLE_OUTLINE: return <Icons.MdAddCircleOutline size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_DELETE: return <Icons.MdDelete size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_DELETE_FOREVER: return <Icons.MdDeleteForever size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_CONTENT_COPY: return <Icons.MdContentCopy size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_NOTIFICATIONS: return <Icons.MdNotifications size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_NOTIFICATIONS_ACTIVE: return <Icons.MdNotificationsActive size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_NOTIFICATIONS_NONE: return <Icons.MdNotificationsNone size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
@@ -69,7 +111,26 @@ switch (name) {
     case iconnames.ICON_KEYBOARD_ARROW_UP: return <Icons.MdKeyboardArrowUp size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_KEYBOARD_ARROW_LEFT: return <Icons.MdKeyboardArrowLeft size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
     case iconnames.ICON_KEYBOARD_ARROW_RIGHT: return <Icons.MdKeyboardArrowRight size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
-
+    case iconnames.ICON_BUG_REPORT: return <Icons.MdBugReport size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_PHOTO_CAMERA: return <Icons.MdPhotoCamera size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_SHORT_TEXT: return <Icons.MdShortText size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_EVENT: return <Icons.MdEvent size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_LIST: return <Icons.MdList size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_WORK: return <Icons.MdWork size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_ACCESS_TIME: return <Icons.MdAccessTime size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_MIC: return <Icons.MdMic size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_VIDEOCAM: return <Icons.MdVideocam size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_CALL: return <Icons.MdCall size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_MESSAGE: return <Icons.MdMessage size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_FILTER_LIST: return <Icons.MdFilterList size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_ATTACH_FILE: return <Icons.MdAttachFile size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_DIRECTIONS: return <Icons.MdDirections size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_MOOD: return <Icons.MdMood size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_CHECK_BOX: return <Icons.MdCheckBox size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_RADIO_BUTTON_CHECKED: return <Icons.MdRadioButtonChecked size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    case iconnames.ICON_GRID_ON: return <Icons.MdGridOn size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
+    
+    case '': return <Icons.MdInfo size={size || this.defaultProps.size} color={this.setColor(this.props.active) || this.defaultProps.color} />
 
     default: return <Icons.MdInfo size={this.defaultProps.size} color={this.defaultProps.color} />
     }
