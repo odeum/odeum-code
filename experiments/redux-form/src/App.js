7f6f30bf93e68bd96b1d3cf1090a7c4d02bc21f6
Christian Broberg <cb@webhouse.dk>
2017-05-22 09:17:43 +0200

    Added new experiment with Redux-Form

--- experiments/redux-form/src/App.js
+++ experiments/redux-form/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Field, reduxForm} from 'redux-form'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+let App= () => {
+  return (
+    <div>
+      <h1>Live Redux-Form Coding</h1>
+      <form action="">
+        <div>
+          <label>First Name</label>
+          <Field name="firstName" component="input" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      </form>
+    </div>
+  )
 }
 
-export default App;
+// Decorated the form
+App = reduxForm({
+    form: 'appForm'
+
+})(App)
+
+export default App
+
