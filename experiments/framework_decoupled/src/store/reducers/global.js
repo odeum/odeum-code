b5128ae24ca6f99ee891b236559b30cd3691bd4b
Alexand3r <andrei.tudor27@yahoo.com>
2017-05-29 11:12:30 +0200

    Dynamic Tab Bug Fix

--- experiments/framework_decoupled/src/store/reducers/global.js
+++ experiments/framework_decoupled/src/store/reducers/global.js
@@ -6,28 +6,28 @@ const initialState = {
     scenes: config.scenes,
     tabChildren: [],
     activeLabel: '',
-    activeScene:''
+    activeScene: ''
 }
 var _ = require('lodash')
 
 export default function global(state = initialState, action) {
     switch (action.type) {
-        case LOAD_DEFAULT_TABS:{
-            let sceneFind = _.find(config.scenes,function(scene){
-                return scene.name=== action.payload
+        case LOAD_DEFAULT_TABS: {
+            let sceneFind = _.find(config.scenes, function (scene) {
+                return scene.name === action.payload
             })
-            let dynamicTabList= _.filter(state.tabChildren,function(tab){
-                 return tab.fixed===true? null : tab
-                })
+            let dynamicTabList = _.filter(state.tabChildren, function (tab) {
+                return tab.fixed === true ? null : tab
+            })
             return {
                 ...state,
-                tabChildren:sceneFind.tabs.concat(dynamicTabList),
-                activeScene:sceneFind.name
+                tabChildren: sceneFind.tabs.concat(dynamicTabList),
+                activeScene: sceneFind.name
             }
         }
         case CLOSE_TAB:
             {
-               
+
                 return {
                     ...state,
                     tabChildren: state.tabChildren.filter((item) => item.label !== action.payload.label)
@@ -35,30 +35,42 @@ export default function global(state = initialState, action) {
             }
 
         case ADD_TAB:
-        {    let formsArray = _.find(state.tabChildren,action.payload)
-            // console.log(formsArray)
-            if(formsArray!==undefined)
-            return {
-                ...state,
-                activeLabel:action.payload.label
-            }
-            else
             {
-                if(state.tabChildren.length===0)
-                return{
-                    ...state,
-                    tabChildren:state.tabChildren.concat(action.payload),
-                    activeLabel:action.payload.label
-                }
-                else
-                {
-                    return{
-                     ...state,
-                    tabChildren:state.tabChildren.concat(action.payload)
+                let formsArray = _.find(state.tabChildren, action.payload)
+                // console.log(formsArray)
+                if (formsArray !== undefined)
+                    return {
+                        ...state,
+                        activeLabel: action.payload.label
+                    }
+                else {
+                    if (state.tabChildren.length === 0) {
+                        let sceneFinder = _.find(config.scenes, function (scene) {
+                            return action.payload.location.includes(scene.name.toLowerCase())
+                        })
+                        if (sceneFinder!==undefined)
+                       { let completeScenes = sceneFinder.tabs.concat(action.payload)
+                            return {
+                                ...state,
+                                tabChildren: state.tabChildren.concat(completeScenes),
+                                activeLabel: action.payload.label
+                            }}
+                        else {
+                            return {
+                                ...state,
+                                tabChildren: state.tabChildren.concat(action.payload),
+                                activeLabel: action.payload.label
+                            }
+                        }
+                    }
+                    else {
+                        return {
+                            ...state,
+                            tabChildren: state.tabChildren.concat(action.payload)
+                        }
                     }
                 }
             }
-        }
         default:
             return state
 
