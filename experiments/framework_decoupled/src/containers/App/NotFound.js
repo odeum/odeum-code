d6ae0432ea7304d6a012bb12e8b5cdc42bf5c053
Alexand3r <andrei.tudor27@yahoo.com>
2017-06-01 11:32:22 +0200

    404 Error Commit Comment Not Found

--- experiments/framework_decoupled/src/containers/App/NotFound.js
+++ experiments/framework_decoupled/src/containers/App/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import * as styled from './styles/NotFoundStyles'
+import HeaderLogo from '../../assets/codejs_logo.png'
 const NotFound = () => {
     return (
-        <div>
-            <h3> Welcome to the dark side! We have cookies </h3>
-        </div>
+        <styled.Div>
+            <styled.ImgDiv><styled.Img src={HeaderLogo} alt="Logo" /></styled.ImgDiv>
+            <styled.e404> 404 Not Found</styled.e404>
+            <styled.ErrMsg> Welcome to the dark side!<br/> We have cookies! </styled.ErrMsg>
+            <styled.ErrMsg2>There's nothing to see here... so you should go back </styled.ErrMsg2>
+        </styled.Div>
     )
 }
 
