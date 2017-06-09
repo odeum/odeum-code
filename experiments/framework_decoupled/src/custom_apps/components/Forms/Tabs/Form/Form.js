3b4702c9ecb1e5e37d4b430a0be304f94b62dc8d
Alexand3r <andrei.tudor27@yahoo.com>
2017-06-09 15:35:00 +0200

    Fix Form

--- experiments/framework_decoupled/src/custom_apps/components/Forms/Tabs/Form/Form.js
+++ experiments/framework_decoupled/src/custom_apps/components/Forms/Tabs/Form/Form.js
@@ -1,41 +1,54 @@
 import React, { Component } from 'react'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import * as formsActions from '../../formsActions'
 // import {browserHistory} from 'react-router'
 class Form extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            formParams:false
+            formParams: false
         }
     }
-    componentWillMount(){
-       let FormProps = this.props.getForm(parseInt(this.props.id,10))
+    componentWillMount() {
+        let FormProps = this.props.getForm(parseInt(this.props.id, 10))
         // console.log(this.state.formParams)
         // console.log(FormProps)
-        if(FormProps!== undefined)
-           this.setState({formParams:true})
-       // else
-        // browserHistory.push('/error404')
-        this.props.updateTab(
-            { label: FormProps.name,
-            location: '/forms/'+this.props.id,
-            icon: 'info',
-            fixed: false}
-        )
+        if (FormProps !== undefined) {
+            this.setState({ formParams: true })
+            this.props.updateTab(
+                {
+                    label: FormProps.name,
+                    location: '/forms/' + this.props.id,
+                    icon: 'info',
+                    fixed: false
+                })
+        }
+        else {
+            //this.setState({ formParams: true })
+            this.props.updateTab(
+                {
+                    label: "Error",
+                    location: '/forms/' + this.props.id,
+                    icon: 'info',
+                    fixed: false
+                })
+        }
+
+
     }
-    render() { return(
-                this.state.formParams ?
+    render() {
+        return (
+            this.state.formParams ?
                 (
-                <div>It worked  {this.props.id}</div>):(<div>Error 404</div>)
+                    <div>It worked  {this.props.id}</div>) : (<div>Error 404</div>)
         )
     }
 }
-const mapStateToProps = (state,ownProps)=>({
-    id:ownProps.params.id
+const mapStateToProps = (state, ownProps) => ({
+    id: ownProps.params.id
 })
-function mapDispatchToProps(dispatch){
-    return bindActionCreators(formsActions,dispatch)
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(formsActions, dispatch)
 }
-export default connect(mapStateToProps,mapDispatchToProps)(Form)
+export default connect(mapStateToProps, mapDispatchToProps)(Form)
