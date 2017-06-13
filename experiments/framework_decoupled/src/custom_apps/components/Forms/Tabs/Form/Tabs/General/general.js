162c1260fcb2c844b3479e593438f47195c23c60
Alexand3r <andrei.tudor27@yahoo.com>
2017-06-13 10:24:25 +0200

    First implementation of Secondary Tab System - NOT DONE

---
+++ experiments/framework_decoupled/src/custom_apps/components/Forms/Tabs/Form/Tabs/General/general.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import * as generalActions from './generalActions.js'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+class General extends Component {
+    componentWillMount() {
+//     this.props.updateTab({
+//     label: 'General',
+//     location: '/dashboard/general',
+//     icon: 'assignment_turned_in',
+//     fixed: true
+// })
+// console.log('------------------------------------')
+// console.log(this.props)
+// console.log('------------------------------------')
+    }
+    render() {
+        return (
+            <div>
+                General
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({})
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(generalActions, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(General)
--- experiments/framework_decoupled/src/custom_apps/components/Forms/Tabs/Form/route.js
+++ experiments/framework_decoupled/src/custom_apps/components/Forms/Tabs/Form/route.js
@@ -1,5 +1,13 @@
 module.exports = {
   path: ':id',
+   getChildRoutes(location, cb) {
+      require.ensure([], (require) => {
+          cb(null, [
+              require('./Tabs/General/route.js'),
+              require('./Tabs/Configuration/route.js')
+          ])
+      })
+  },
   getComponent(location, cb) {
     require.ensure([], (require) => {
       cb(null, require('./Form.js').default)
