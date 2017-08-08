28e0f724c4af28202ff9c01eed410a076205265e
Henrik Hansen <hh@fsck.dk>
2017-08-08 22:15:30 +0200

    Add button component from experiment

--- experiments/framework_decoupled_v3/src/framework/assets/fonts.js
+++ experiments/framework_decoupled_v3/src/framework/assets/fonts.js
@@ -2,5 +2,15 @@
 
 export const MAIN = 'Source Sans Pro'
 export const PRIMARY = 'Source Sans Pro'
+export const DEFAULT = 'Source Sans Pro'
+
 export const NORMAL = 'Source Sans Pro'
 
+export const SIZE_DEFAULT = '18px'
+export const SIZE_EXTRA_SMALL = '7px'
+export const SIZE_SMALL = '9px'
+export const SIZE_LARGE = '24px'
+
+export const FONT_WEIGHT = '300'
+
+export const FONT_STYLE = 'normal' /*(normal | italic | oblique)*/
--- experiments/framework_decoupled_v3/src/framework/assets/theme.js
+++ experiments/framework_decoupled_v3/src/framework/assets/theme.js
@@ -12,7 +12,30 @@ const theme = {
       TAB_SELECTED:colors.TAB_SELECTED
   },
   button: {
-    color: colors.TAB_COLOR_1,
+      color: colors.BUTTON_TEXT,
+      background: colors.BUTTON_DEFAULT,
+  },
+  buttonSize: {
+      large: {
+          size: fonts.SIZE_LARGE,
+          padding: '9px 20px 9px 15px', /* top, right, buttom, left */
+          lineHeight: 1.9
+      },
+      default: {
+          size: fonts.SIZE_DEFAULT,
+          padding: '9px 20px 9px 15px', /*9px 20px 9px 15px; *//* top, right, buttom, left */
+          lineHeight: 1.9
+      },
+      small: {
+          size: fonts.SIZE_SMALL,
+          padding: '9px 20px 9px 15px', /* top, right, buttom, left */
+          lineHeight: 1.9
+      },
+      xsmall: {
+          size: fonts.SIZE_EXTRA_SMALL,
+          padding: '9px 20px 9px 15px', /* top, right, buttom, left */
+          lineHeight: 1.9
+      }
   },
   breakpoints: [ 32, 48, 64, 80 ] //640,830,1024,1280 px
 }
