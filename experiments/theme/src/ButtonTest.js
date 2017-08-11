3c62fb9cebac2557204621c8bce72bc4bc60ee20
Christian Broberg <cb@webhouse.dk>
2017-08-11 13:06:51 +0200

    Theme linting

--- experiments/theme/src/ButtonTest.js
+++ experiments/theme/src/ButtonTest.js
@@ -11,12 +11,12 @@ import { Text } from './components/Styles'
 
 
 export class ButtonTest extends PureComponent {
-    render() {
-        return (
+	render() {
+		return (
             <div>
                 <StyledButton><Icon icon='dashboard' size={18} color={colors.COMP_BLUE} active={true} style={theme.iconStyle} />Dashboard</StyledButton>
                 <StyledButton><Icon icon={iconname.ICON_MAIL_OUTLINE} size={18} color={colors.BUTTON_TEXT} active={true} style={theme.iconStyle} />Task New Meeting</StyledButton>
-                <StyledButton><Icon icon={iconname.ICON_ASSIGNMENT} size={18} color={colors.COMP_ORANGE} active={true} style={{verticalAlign: '-3px', paddingRight: '16px'}} />Styles overridden</StyledButton>
+                <StyledButton><Icon icon={iconname.ICON_ASSIGNMENT} size={18} color={colors.COMP_ORANGE} active={true} style={{ verticalAlign: '-3px', paddingRight: '16px' }} />Styles overridden</StyledButton>
                 
                 <Button icon={iconname.ICON_MAIL_OUTLINE} size={18}>Mail</Button>
                 <Button icon={iconname.ICON_CHECK_CIRCLE} size={18}>Save</Button>
@@ -24,16 +24,16 @@ export class ButtonTest extends PureComponent {
                 <Button icon={iconname.ICON_CHECK_CIRCLE} size={18}>Normal themed button</Button>
                 <Button icon={iconname.ICON_LAPTOP_MAC} size={18}>Button Component</Button>
             </div>
-        )
-    }
+		)
+	}
 }
 
 export const ButtonTest2 = () => {
-    return (
+	return (
         <div>
             <StyledButton><Icon icon='dashboard' size={18} color={colors.TAB_COLOR_6} active={true} style={theme.iconStyle} />Dashboard</StyledButton>
             <StyledButton><Icon icon={iconname.ICON_MAIL_OUTLINE} size={18} color={colors.BUTTON_TEXT} active={true} style={theme.iconStyle} />Task New Meeting</StyledButton>
-            <StyledButton><Icon icon={iconname.ICON_ASSIGNMENT} size={18} color={colors.TAB_COLOR_5} active={true} style={{verticalAlign: '-3px', paddingRight: '6px'}} />Forms</StyledButton>
+            <StyledButton><Icon icon={iconname.ICON_ASSIGNMENT} size={18} color={colors.TAB_COLOR_5} active={true} style={{ verticalAlign: '-3px', paddingRight: '6px' }} />Forms</StyledButton>
             
             <Button icon={iconname.ICON_MAIL_OUTLINE} size={18}><Text>Styled text</Text></Button>
             <Button icon={iconname.ICON_CHECK_CIRCLE} size={18}>Save</Button>
@@ -41,5 +41,5 @@ export const ButtonTest2 = () => {
             <Button icon={iconname.ICON_CLOUD} size={18}>Stateless component</Button>
             <Button size={18}>No icon</Button>
         </div>
-    )
+	)
 }
