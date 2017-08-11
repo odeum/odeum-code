094483f5d08ec37dab8c6c3b7d9c557bdd4679b0
Christian Broberg <cb@webhouse.dk>
2017-08-11 14:51:07 +0200

    Testing linting

--- experiments/theme/src/components/ButtonComp.js
+++ experiments/theme/src/components/ButtonComp.js
@@ -8,55 +8,55 @@ import { ICON_DEFAULT_COLOR } from './colors'
 
 class Button extends React.Component {
 
-constructor(props) {
-    super(props)
-    this.defaultProps = {
-        name: 'info',
-        size: 75,
-        color: ICON_DEFAULT_COLOR,
-        active: false,
-        style: {
-            verticalAlign: 'middle'
-        }
-    }    
-  }
+	constructor(props) {
+		super(props)
+		this.defaultProps = {
+			name: 'info',
+			size: 75,
+			color: ICON_DEFAULT_COLOR,
+			active: false,
+			style: {
+				verticalAlign: 'middle'
+			}
+		}    
+	}
 
-setColor(active) {
-    if(active === true)
-        {return(this.props.color)}
-    else
-        {return(this.defaultProps.color)}
-}
+	setColor(active) {
+		if (active === true)
+        {return (this.props.color)}
+		else
+        {return (this.defaultProps.color)}
+	}
 
-getSubComponent(name, size) {
+	getSubComponent(name, size) {
 // All theme available icons must be in this switch
 
-switch (name) {    
-    case iconname.ICON_MENU: {
-        return <Icons.MdMenu 
+		switch (name) {    
+			case iconname.ICON_MENU: {
+				return <Icons.MdMenu 
             size={size || this.defaultProps.size} 
             color={this.setColor(this.props.active) || this.defaultProps.color} 
             style={this.defaultProps.style} />
-    }
+			}
         
-    default: return <Icons.MdInfo size={this.defaultProps.size} color={this.defaultProps.color} />
-    }
-}    
-    render() {
-        return (
+			default: return <Icons.MdInfo size={this.defaultProps.size} color={this.defaultProps.color} />
+		}
+	}    
+	render() {
+		return (
             this.getSubComponent(this.props.icon, this.props.size)
-        )        
-    }
+		)        
+	}
 }
 
 Button.propTypes = {
-  name: PropTypes.string.isRequired,
-  size: PropTypes.number,
-  color: PropTypes.string, 
-  active: PropTypes.bool,
-  style: PropTypes.shape({
-    verticalAlign: PropTypes.string
-  })
+	name: PropTypes.string.isRequired,
+	size: PropTypes.number,
+	color: PropTypes.string, 
+	active: PropTypes.bool,
+	style: PropTypes.shape({
+		verticalAlign: PropTypes.string
+	})
 }
 
 export default Button
