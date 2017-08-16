020ad904ae6a63cd295fab966c98955f63b8b90b
Henrik Hansen <hh@fsck.dk>
2017-08-16 14:16:39 +0200

    initial work on image handler

--- experiments/framework_decoupled_v3/src/app/containers/eplan-appendix/Appendix/Editors/Quill.js
+++ experiments/framework_decoupled_v3/src/app/containers/eplan-appendix/Appendix/Editors/Quill.js
@@ -4,24 +4,35 @@ import ReactQuill from 'react-quill'
 const Quill = ({ value, onChange }) => {
 	return (
 		<div style={{ width: '100%' }}>
-
 			<ReactQuill value={value} 
 				onChange={onChange}
-				modules={Quill.modules}/>
+				modules={Quill.modules} />
 		</div>
 	)
 }
 
 export default Quill
 
+
 Quill.modules = {
-	toolbar: [
-		[{ 'header': '1' }, { 'header': '2' }],
-		[{ size: [] }],
-		['bold', 'italic', 'underline', 'strike', 'blockquote'],
-		[{ 'list': 'ordered' }, { 'list': 'bullet' }, 
-			{ 'indent': '-1' }, { 'indent': '+1' }],
-		['link', 'image', 'video'],
-		['clean']
-	]
+	toolbar: {
+		container: [
+			[{ 'header': '1' }, { 'header': '2' }],
+			[{ size: [] }],
+			['bold', 'italic', 'underline', 'strike', 'blockquote'],
+			[{ 'list': 'ordered' }, { 'list': 'bullet' }, 
+				{ 'indent': '-1' }, { 'indent': '+1' }],
+			['link', 'image', 'video'],
+			['clean']
+		],
+		handlers: {
+			"image": imageHandler,
+	  	}
+	}
+}
+
+function imageHandler() {
+//	var range = this.quill.getSelection()
+//	var value = prompt('What is the image URL')
+//	this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER)
 }
