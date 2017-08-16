0290bd5b0b3b592b3c216f4c713a95d1f1ea78ab
Henrik Hansen <hh@fsck.dk>
2017-08-16 15:33:40 +0200

    fix linting

--- experiments/framework_decoupled_v3/src/framework/assets/colors.js
+++ experiments/framework_decoupled_v3/src/framework/assets/colors.js
@@ -38,7 +38,7 @@ export const MENUBAR_SELECTED = '#81C1EA'
 export const TAB = '#E3E5E5'
 export const TAB_HOVER = '#81C1EA'
 export const TAB_SELECTED = '#3B97D3'
-export const TAB_TEXT ='#5E5E5E'
+export const TAB_TEXT = '#5E5E5E'
 export const TAB_TEXT_SELECTED = '#FFFFFF'
 
 // SEARCH COLORS
