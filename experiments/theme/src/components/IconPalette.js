2c8494aaf3a0d2cf68261390e80282d22e9b5e92
Christian Broberg <cb@webhouse.dk>
2017-08-24 16:25:06 +0200

    Removed require('typeface-source-sans-pro') which relies on Normalize and this breaks react-select

--- experiments/theme/src/components/IconPalette.js
+++ experiments/theme/src/components/IconPalette.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
 
 import { iconArr } from './icon_array'
+import * as Icons from 'react-icons/lib/md'
 
 
 class IconPalette extends Component {
+
 	render() {
+		let TagName = ''
 		return (
-            <div>
-                {iconArr.map((icon) => (
-                    <div key={icon.name}>
-						{icon.name} {icon.value} {icon.component()}
-                    </div>
-                ))}
-            </div>
+			<div>
+				{iconArr.map((icon) => {
+					TagName = icon.component()
+					console.log(TagName)
+					console.log(Icons)
+					return (
+						<div key={icon.name}>
+							{icon.name} {icon.value} {icon.component()}
+							{/* <Icons.TagName /> */}
+						</div>
+					)
+				})}
+			</div>
 		)
 	}
 }
