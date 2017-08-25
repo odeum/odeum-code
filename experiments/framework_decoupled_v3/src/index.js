eeb93809cf77a3b0f8be07fd521afb323b37eca7
Henrik Hansen <hh@fsck.dk>
2017-08-25 09:52:57 +0200

    deactivate normalize

--- experiments/framework_decoupled_v3/src/index.js
+++ experiments/framework_decoupled_v3/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 import AppContainer from 'framework/containers/App/App'
 import { history, store } from 'framework/store'
-
-import 'normalize.css'
+// import 'normalize.css'
 require('typeface-source-sans-pro')
+
 ReactDOM.render(
 	<AppContainer store = { store } history = { history } />
 	,
