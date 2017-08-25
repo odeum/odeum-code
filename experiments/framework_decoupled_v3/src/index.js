b4d5118a5eaeec3163ee7f7f968e03d2e2602be9
Alexand3r <andrei.tudor27@yahoo.com>
2017-08-25 09:46:45 +0200

    Re-added TinyMCE APIKey and readded normalize and typeface

--- experiments/framework_decoupled_v3/src/index.js
+++ experiments/framework_decoupled_v3/src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 import AppContainer from 'framework/containers/App/App'
 import { history, store } from 'framework/store'
-//BUG normalize is not compatible with react-select
-//TODO Replace react-select with a home made drop down
-// import 'normalize.css'
-// require('typeface-source-sans-pro')
+
+import 'normalize.css'
+require('typeface-source-sans-pro')
 ReactDOM.render(
 	<AppContainer store = { store } history = { history } />
 	,
