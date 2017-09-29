92fcd99d805899ca8bf434e3934d7ebc03295a92
Mikkel Broberg <mb@webhouse.dk>
2017-09-29 09:32:37 +0200

    Login added

--- experiments/framework_decoupled_v3/src/framework/components/Login/Login.js
+++ experiments/framework_decoupled_v3/src/framework/components/Login/Login.js
@@ -3,21 +3,76 @@ import { Img, LoginLabel, BackgroundDiv, HeaderDiv, Input, InputDiv, LoginButton
 import HeaderLogo from '../../assets/eplan_logo.png'
 
 
-const Login = ({ onSubmit }) => {
-	return (
-		<BackgroundDiv>
-			<HeaderDiv><Img src={HeaderLogo}/></HeaderDiv>
-			<InputDiv>
-				<LoginLabel>Username</LoginLabel>
-				<Input/>
-			</InputDiv>
-			<InputDiv>
-				<LoginLabel>Password</LoginLabel>
-				<Input type='password'/>
-			</InputDiv>
-			<LoginButton onClick={(e) => {e.preventDefault();onSubmit()}}>Login</LoginButton>
-		</BackgroundDiv>
-	)
+// const Login__ = ({ onSubmit }) => {
+// 	return (
+// 		<BackgroundDiv>
+// 			<HeaderDiv><Img src={HeaderLogo}/></HeaderDiv>
+// 			<InputDiv>
+// 				<LoginLabel>Username</LoginLabel>
+// 				<Input id="username"/>
+// 			</InputDiv>
+// 			<InputDiv>
+// 				<LoginLabel>Password</LoginLabel>
+// 				<Input type='password'/>
+// 			</InputDiv>
+// 			<LoginButton onClick={(e) => {e.preventDefault();onSubmit()}}>Login</LoginButton>
+// 		</BackgroundDiv>
+// 	)
+// }
+
+class Login extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			username: '',
+			password: ''
+		}
+		this.handleInputChange = this.handleInputChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleInputChange(event) {
+		const target = event.target
+		const value = target.type === 'checkbox' ? target.checked : target.value
+		const name = target.name
+
+		this.setState({
+			[name]: value
+		})
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+		this.props.onSubmit({ ...this.state })
+	  }
+
+	render() {
+		return (
+			<BackgroundDiv>
+				<form>
+				<HeaderDiv><Img src={HeaderLogo}/></HeaderDiv>
+				<InputDiv>
+					<LoginLabel>Username</LoginLabel>
+					<Input 
+						name="username"
+						type="text"
+						value={this.state.username}
+						onChange={this.handleInputChange}/>
+				</InputDiv>
+				<InputDiv>
+					<LoginLabel>Password</LoginLabel>
+					<Input 
+						name="password"
+						type="password"
+						value={this.state.password}
+						onChange={this.handleInputChange} />
+				</InputDiv>
+				<LoginButton onClick={this.handleSubmit}>Login</LoginButton>
+				</form>
+			</BackgroundDiv>
+		)
+	}
 }
+
 
 export default Login
--- experiments/framework_decoupled_v3/src/framework/containers/Home/Home.js
+++ experiments/framework_decoupled_v3/src/framework/containers/Home/Home.js
@@ -21,25 +21,30 @@ import { connect } from 'react-redux'
 import LoginContainer from 'framework/containers/Login/Login'
 
 //REFACTOR
-import { getAppendixCfg } from 'app/store/modules/eplan'
+import { getAppendixCfg, doMyLogin, doCookieLogin } from 'app/store/modules/eplan'
 
 class Home extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			loggedIn: true
+			loggedIn: false
 		}
 		this.handleLogin = this.handleLogin.bind(this)
 		this.props.onMount()
 	}
-	handleLogin() {
-		this.setState({ loggedIn: true })
+
+	async componentWillMount() {
+		await this.props.auth()
+	}
+
+	async handleLogin(data) {
+		await this.props.login(data)
 	}
 	render() {
 		return (
 
 			<ThemeProvider theme={theme}>
-				{this.state.loggedIn ?
+				{this.props.loggedIn ?
 					<div>
 						<HomeDiv>
 							<HeaderContainer />
@@ -63,7 +68,9 @@ Home.propTypes = {
 }
 
 const mapStateToProps = (state, ownProps) => ({
-	activeScene: state.tabReducer.activeScene
+	activeScene: state.tabReducer.activeScene,
+	authObj: state.eplan.authObj,
+	loggedIn: (state.eplan.authObj) ? state.eplan.authObj.isLoggedIn : false
 })
 
 function mapDispatchToProps(dispatch) {
@@ -71,6 +78,13 @@ function mapDispatchToProps(dispatch) {
 		onMount: () => {
 			dispatch(getAppendixCfg())
 			// dispatch(replace('/eplan/list'))
+		},
+		login: async (data) => {
+			dispatch(await doMyLogin(data))
+		},
+		auth: async () => {
+			dispatch(await doCookieLogin())
+			
 		}
 	}
 
