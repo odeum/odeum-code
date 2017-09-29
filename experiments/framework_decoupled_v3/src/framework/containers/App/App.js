dcd78bfdb042494cfd11cdfb92dfb8db2445a695
Henrik Hansen <hh@fsck.dk>
2017-09-29 09:58:14 +0200

    readd toast css

--- experiments/framework_decoupled_v3/src/framework/containers/App/App.js
+++ experiments/framework_decoupled_v3/src/framework/containers/App/App.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router'
 import rootRoute from './rootRoute'
-// import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-toastify/dist/ReactToastify.min.css'
 require('typeface-source-sans-pro')
 
 class AppContainer extends Component {
