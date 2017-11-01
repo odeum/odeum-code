ff02930da78d56b977db85b4eeab2472790e7fd4
Henrik Hansen <hh@fsck.dk>
2017-11-01 14:27:44 +0100

    fix delete modal

--- experiments/framework_decoupled_v3/src/app/components/eplan-appendix/Appendix/DeleteModal.js
+++ experiments/framework_decoupled_v3/src/app/components/eplan-appendix/Appendix/DeleteModal.js
@@ -1,32 +1,31 @@
 import React from 'react'
-import { ModalWindow, ModalHeader, ModalContent, ModalHeaderIcon, ModalHeaderTitle, ModalHeaderClose, ModalButtonPanel } from 'framework/components/styles/ModalStyles'
-import Button from 'framework/components/Widgets/Button'
+import { ModalWindow, ModalHeader, ModalContent, ModalHeaderIcon, ModalHeaderTitle, ModalHeaderClose } from 'framework/components/styles/ModalStyles'
 import * as iconname from 'framework/assets/icons'
 import * as colors from 'framework/assets/colors'
 import Icon from 'framework/assets/Icon'
+import { Button, ButtonPanel } from 'odeum-ui'
 
 const DeleteModal = ({ deleteModalIsOpen, closeDeleteModal, customStyles, appendix, onClickDeleteAppendix }) => {
 	return (
 		<div>
 			{appendix !== null ?
-				<ModalWindow isOpen={deleteModalIsOpen} onRequestClose={closeDeleteModal} contentLabel="Eksportér til plansystem">
+				<ModalWindow isOpen={deleteModalIsOpen} onRequestClose={closeDeleteModal} contentLabel="Slet tillæg">
 					<ModalHeader>
 						<ModalHeaderIcon>
 							<Icon icon={iconname.ICON_DELETE} size={30} color={colors.MODAL_HEADER_ICON} active={true} />
 						</ModalHeaderIcon>
-						<ModalHeaderTitle>Eksportér til plansystem</ModalHeaderTitle>
+						<ModalHeaderTitle>Slet tillæg</ModalHeaderTitle>
 						<ModalHeaderClose onClick={closeDeleteModal}>
 							<Icon icon={iconname.ICON_CLOSE} size={30} color={colors.MODAL_HEADER_ICON} active={true} />
 						</ModalHeaderClose>
 					</ModalHeader>
 					<ModalContent>
-						<div id="exportStepOne">
-                        Slet tillæg {appendix.name}!
-							<ModalButtonPanel>                            
-								<Button id="deleteButton" onClick={onClickDeleteAppendix} icon={iconname.ICON_DELETE} size={18}>OK</Button>
-								<Button id="deleteCloseButton" onClick={closeDeleteModal} icon={iconname.ICON_CLOSE} size={18}>Luk</Button>
-							</ModalButtonPanel>
-						</div>
+						Er du sikker på du vil slette tillæg <b>{appendix.name}</b>?
+						<br />
+						<ButtonPanel justify="left">                            
+							<Button id="deleteCloseButton" onClick={closeDeleteModal} icon="close" label="Nej" />
+							<Button id="deleteButton" onClick={onClickDeleteAppendix} icon="delete" label="Ja" />
+						</ButtonPanel>
 					</ModalContent>
 				</ModalWindow>
 				: ""
